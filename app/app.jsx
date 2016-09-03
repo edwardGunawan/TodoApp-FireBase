77513633de5ec8bce151be97571ab2d26a50aaa1
Edward Huang <egunawan@uscd.edu>
2016-09-03 15:08:53 -0700

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
 var TodoApp = require('TodoApp');
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+/* listen to changes on my store */
+store.subscribe(() => {
+  console.log('New State', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 
 // Load foundation
 // after includePaths in webpack config for telling the sass loader to include the file, we don't need to have it
---
+++ app/store/configureStore.jsx
@@ -0,0 +1,16 @@
+var redux = require('redux');
+var {searchTextReducer, showCompletedReducer, todoReducer} = require('reducers');
+
+export var configure = () => {
+  var reducers = redux.combineReducers({
+    searchText: searchTextReducer,
+    showCompleted: showCompletedReducer,
+    todos: todoReducer
+  });
+
+  var store = redux.createStore(reducers, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+
+  return store; // to pass it back and set it inside of the store variable that we created in app.jsx
+}
